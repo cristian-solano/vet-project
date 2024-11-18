151208667c33af8d3a355f03cad86a6c7c38ab32
CristianSolano <cristiansolano1941@gmail.com>
2024-11-17 22:39:29 -0500

    delete component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Error from './Pages/Error';
-import Register from './Pages/Register';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Login/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
-        <Route path="/register" element={<Register/>}></Route>
         <Route path="/*" element={<Error></Error>}></Route>
       </Routes>
       </div>
